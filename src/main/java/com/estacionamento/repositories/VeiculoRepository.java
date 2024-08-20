b6f4128580a6d08b28451c5f1c771f72928c1db5
Guilherme Dias <guidias200220@gmail.com>
2024-08-20 11:27:51 -0300

    feat: create function findBySaidaNull

--- src/main/java/com/estacionamento/repositories/VeiculoRepository.java
+++ src/main/java/com/estacionamento/repositories/VeiculoRepository.java
@@ -3,9 +3,12 @@ package com.estacionamento.repositories;
 import com.estacionamento.domain.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface VeiculoRepository extends JpaRepository<Veiculo, UUID> {
     Optional<Veiculo> findVeiculoByPlaca(String placa);
+    List<Veiculo> findBySaidaNull();
 }
