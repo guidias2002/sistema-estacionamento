d25e15efc1d05afb001bd3fdf0f84a50976553ed
Guilherme Dias <guidias200220@gmail.com>
2024-08-28 11:48:27 -0300

    feat: update migrations

--- src/main/resources/db/migration/V1__create-table-veiculos.sql
+++ src/main/resources/db/migration/V1__create-table-veiculos.sql
@@ -1,9 +1,11 @@
 CREATE TABLE veiculos (
     id UUID PRIMARY KEY NOT NULL,
     tipoVeiculo TEXT NOT NULL,
-    placa VARCHAR(8) UNIQUE NOT NULL,
+    placa VARCHAR(8) NOT NULL,
     modelo TEXT NOT NULL,
     cor TEXT NOT NULL,
     entrada TIMESTAMP NOT NULL,
-    saida TIMESTAMP
+    saida TIMESTAMP,
+    valor TEXT,
+    periodoEmMinutos INTEGER
 );
